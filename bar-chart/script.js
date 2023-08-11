284c4d8b400f870fb4febe5f4caefdad13c39a0a
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-08-11 13:34:29 +0300

    feat: add overlay elem with class "overlay" - barChart

--- bar-chart/script.js
+++ bar-chart/script.js
@@ -8,4 +8,9 @@ const tooltip = d3
     .attr('id', 'tooltip')
     .style('opacity', 0);
 
+    const overlay = d3
+    .select('.chartContainer')
+    .append('div')
+    .attr('class', 'overlay')
+    .style('opacity', 0);
 
