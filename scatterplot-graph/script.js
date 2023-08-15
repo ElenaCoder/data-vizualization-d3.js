2ac6080211b6591a8e62a632f8801e166c060441
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-08-15 15:02:21 +0300

    feat: define the chart margin and dimensions-scatterplot

---
+++ scatterplot-graph/script.js
@@ -0,0 +1,15 @@
+// Define the data source URL
+const url =
+    'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
+
+// Define the chart margin and dimensions
+const margin = {
+    top: 60,
+    right: 30,
+    bottom: 30,
+    left: 60,
+};
+const width = 920 - margin.right - margin.left;
+const height = 630 - margin.top - margin.bottom;
+
+
